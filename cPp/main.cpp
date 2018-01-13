5f5e861f141f49df32d47ee6d6b7de0af67b8fd6
RbBtSn0w <hamiltonsnow@gmail.com>
2018-01-13 23:01:08 +0800

    Initial Commit

---
+++ cPp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cPp
+//
+//  Created by Snow.Y.Wu on 1/13/18.
+//  Copyright © 2018 rbbtsn0w. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
